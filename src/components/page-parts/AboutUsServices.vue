e96faf7278af6a37af702c72e63c4dba8403bf59
Ed Tewiah <etewiah@hotmail.cim>
2018-04-17 18:17:46 +0200

    Fix aboutUs positioning

--- src/components/page-parts/AboutUsServices.vue
+++ src/components/page-parts/AboutUsServices.vue
@@ -1,6 +1,6 @@
 <template>
   <section v-if="blockContents" fluid>
-    <v-layout column wrap class="my-5" align-center>
+    <v-layout container wrap class="my-5" align-center>
       <v-flex xs12 sm4 class="my-3">
         <div class="text-xs-center">
           <h2 class="headline"></h2>
